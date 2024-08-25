73e7b28f2a8d9bec8d636d1a1e9a822da9e6a666
rougsig <rougsig@gmail.com>
2024-08-25 18:53:31 +0400

    Add missed files for template

--- instances/cli/src/__tests__/calc.test.ts
+++
@@ -1,6 +0,0 @@
-import {sum} from '#/calc'
-import {expect, test} from 'vitest'
-
-test('sum 2 + 2 = 4', () => {
-  expect(sum(2, 2)).toEqual(4)
-})
---
+++ instances/cli/src/__tests__/readme-md.test.ts
@@ -0,0 +1,5 @@
+import {expect, test} from 'vitest'
+
+test('test for README.md', () => {
+  expect(2 + 4).toEqual(4)
+})
